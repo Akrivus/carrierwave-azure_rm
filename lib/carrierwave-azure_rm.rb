cc347f0dc872edc900f750a01ddd41e1d3e94d8e
Owen Adrian <akrivus@gmail.com>
2020-07-16 21:41:40 -0700

    Added storage container back, forgot I removed it on accident.

--- lib/carrierwave-azure_rm.rb
+++ lib/carrierwave-azure_rm.rb
@@ -4,6 +4,7 @@ require 'carrierwave/storage/azure_rm'
 
 class CarrierWave::Uploader::Base
   add_config :azure_storage_connection
+  add_config :azure_container
   add_config :public_access_level
   add_config :auto_sign_urls
   add_config :token_expire_after
